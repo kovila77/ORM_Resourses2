5ac4311b9fb212e314de111a2500bbb24cd409c4
kovila77 <354981@mail.ru>
2020-05-09 09:31:47 +0500

    tmp

---
+++ DataGridViewComboBoxColumnResources.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ORM_Resourses
+{
+    class DataGridViewComboBoxColumnResources : DataGridViewComboBoxColumn
+    {
+
+        private DataTable _dtBuildings = null;
+
+        public DataGridViewComboBoxColumnResources() : base()
+        {
+            this.Name = MyHelper.strBuildingId;
+            this.HeaderText = "Форпост";
+            this.DisplayMember = MyHelper.strBuildingName;
+            this.ValueMember = MyHelper.strBuildingId;
+            this.DataPropertyName = MyHelper.strBuildingId;
+            this.FlatStyle = FlatStyle.Flat;
+            InitializeDataTableBuildings();
+        }
+
+        public void InitializeDataTableBuildings()
+        {
+            _dtBuildings = new DataTable();
+            _dtBuildings.Columns.Add(MyHelper.strBuildingId, typeof(int));
+            _dtBuildings.Columns.Add(MyHelper.strBuildingName, typeof(string));
+            this.DataSource = _dtBuildings;
+        }
+
+        public void Add(int building_id, string building_name, int? outpost_id)
+        {
+            //_dtOutposts.Rows.Add(outpost_id, outpost_name, outpost_coordinate_x, outpost_coordinate_y, outpost_coordinate_z);
+            _dtBuildings.Rows.Add(building_id, building_name + (outpost_id.HasValue ? " — "
+                                             + outpost_id.ToString() : ""));
+        }
+
+        public void Change(int building_id, string building_name, int? outpost_id = null)
+        {
+            DataRow forChange = _dtBuildings.AsEnumerable().SingleOrDefault(row => row.Field<int>(MyHelper.strBuildingId) == building_id);
+            if (forChange != null)
+            {
+                forChange[MyHelper.strBuildingName] = building_name + (outpost_id.HasValue ? " — "
+                                                                    + outpost_id.ToString() : "");
+            }
+        }
+
+        public void Remove(int building_id)
+        {
+            DataRow forDel = _dtBuildings.AsEnumerable().SingleOrDefault(row => row.Field<int>(MyHelper.strBuildingId) == building_id);
+            if (forDel != null)
+            {
+                _dtBuildings.Rows.Remove(forDel);
+            }
+        }
+    }
+}
---
+++ MyHelper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ORM_Resourses
+{
+    public static class MyHelper
+    {
+        public static readonly string strEmptyCell = "Вы ввели пустое значение!";
+        public static readonly string strUncorrectIntValueCell = "Вы ввели некорректное число!";
+        public static readonly string strExistingMision = "Для данного форпоста существует точная копия ведённой миссии!";
+        public static readonly string strBadRow = "Плохая строка!";
+        public static readonly string strOutpostIsUsing = "Данный форпост связан с миссией! Измените или удалите миссию!";
+
+        public static readonly string strOutpostId = "outpost_id";
+        public static readonly string strOutpostName = "outpost_name";
+        public static readonly string strOutpostEconomicValue = "outpost_economic_value";
+        public static readonly string strOutpostCoordinateX = "outpost_coordinate_x";
+        public static readonly string strOutpostCoordinateY = "outpost_coordinate_y";
+        public static readonly string strOutpostCoordinateZ = "outpost_coordinate_z";
+
+        public static readonly string strMissionId = "mission_id";
+        public static readonly string strMissionDescription = "mission_description";
+        public static readonly string strDateBegin = "date_begin";
+        public static readonly string strDatePlanEnd = "date_plan_end";
+        public static readonly string strDateActualEnd = "date_actual_end";
+
+        public static readonly string strBuildingId = "building_id";
+        public static readonly string strBuildingName = "building_name";
+
+        public static readonly string strResourceId = "resources_id";
+        public static readonly string strResourceName = "resources_name";
+
+        //public static readonly string strUniqueOutpostConstraintName = "unique_outpost";
+
+        public static string RmvExtrSpaces(this String str)
+        {
+            if (str == null) return str;
+            str = str.Trim();
+            str = Regex.Replace(str, @"\s+", " ");
+            return str;
+        }
+
+        public static bool IsEntireRowEmpty(DataGridViewRow row)
+        {
+            foreach (DataGridViewCell cell in row.Cells)
+                if (cell.FormattedValue.ToString().RmvExtrSpaces() != "")
+                    return false;
+            return true;
+        }
+    }
+}
